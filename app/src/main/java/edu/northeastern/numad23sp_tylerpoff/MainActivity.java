11b44ec0d5532b92f5662f76708447fb2d5f85e0
TylerPoff <Tyler.Poff1900@gmail.com>
2023-01-12 21:38:08 -0500

    Build app with About Me button

--- app/src/main/java/edu/northeastern/numad23sp_tylerpoff/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_tylerpoff/MainActivity.java
@@ -3,6 +3,9 @@ package edu.northeastern.numad23sp_tylerpoff;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn_about_me = findViewById(R.id.btnAboutMe);
+        btn_about_me.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Name: Tyler Poff\nEmail: poff.t@northeastern.edu"
+                , Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
