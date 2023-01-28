a2cbdc92b8f84e8fe0de66cbd44984d2eebb9467
TylerPoff <Tyler.Poff1900@gmail.com>
2023-01-28 15:05:14 -0500

    - Added ClickyClicky button to main - ClickyClikcy opens new activity - Added 6 Buttons labeled A,B,C,D,E,F to ClickyClicky activity

--- app/src/main/java/edu/northeastern/numad23sp_tylerpoff/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_tylerpoff/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn_about_me = findViewById(R.id.btnAboutMe);
-        btn_about_me.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Name: Tyler Poff\nEmail: poff.t@northeastern.edu"
-                , Toast.LENGTH_LONG).show();
-            }
-        });
-
+        Button btn_clicky_clicky = findViewById(R.id.btnClickyClicky);
+        btn_about_me.setOnClickListener(v -> Toast.makeText(getApplicationContext(), "Name: Tyler Poff\nEmail: poff.t@northeastern.edu"
+        , Toast.LENGTH_LONG).show());
+        btn_clicky_clicky.setOnClickListener(v -> openActivityClickyClicky());
+    }
+    public void openActivityClickyClicky() {
+        Intent intentClickyClicky = new Intent(this, ClickyClicky.class);
+        startActivity(intentClickyClicky);
     }
 }
