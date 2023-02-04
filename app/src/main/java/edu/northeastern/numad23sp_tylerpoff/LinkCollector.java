b53499ab3d1b4a1ea3137cc18d19582a80b367ed
TylerPoff <Tyler.Poff1900@gmail.com>
2023-02-03 21:39:06 -0500

    - Finalized Link Collector

--- app/src/main/java/edu/northeastern/numad23sp_tylerpoff/LinkCollector.java
+++ app/src/main/java/edu/northeastern/numad23sp_tylerpoff/LinkCollector.java
@@ -1,14 +1,19 @@
 package edu.northeastern.numad23sp_tylerpoff;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +21,12 @@ public class LinkCollector extends AppCompatActivity {
     private static final String STATE_LINKS = "links";
     RecyclerView linkRecyclerView;
     List<Link> links;
+    private AlertDialog.Builder dialogEntry;
+    private EditText newName;
+    private EditText newUrl;
+    private Button linkAdd;
+    private Button linkCancel;
+    private AlertDialog dialogBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +38,68 @@ public class LinkCollector extends AppCompatActivity {
             links = new ArrayList<>();
         }
         setContentView(R.layout.activity_link_collector);
-        FloatingActionButton addLink = findViewById(R.id.btnCollector);
-
-        links.add(new Link("test1", "test2"));
-
+        addLink_FAB();
         linkRecyclerView = findViewById(R.id.link_collector_rv);
         linkRecyclerView.setHasFixedSize(true);
         linkRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         linkRecyclerView.setAdapter(new LinkAdapter(links, this));
     }
-}
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable(STATE_LINKS, (Serializable) links);
+    }
+
+    public void addLink(String name, String url) {
+        links.add(new Link(name, url));
+    }
+
+    public void addLink_FAB() {
+        FloatingActionButton addFAB = findViewById(R.id.btnCollector);
+        addFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createDialogEntry();
+            }
+        });
+
+    }
+
+    public void createDialogEntry() {
+        dialogEntry = new AlertDialog.Builder(this);
+        final View linkEntry = getLayoutInflater().inflate(R.layout.link_dialog, null);
+        newName = linkEntry.findViewById(R.id.LinkNameET);
+        newUrl = linkEntry.findViewById(R.id.LinkURLET);
+        linkAdd = linkEntry.findViewById(R.id.btnLinkAdd);
+        linkCancel = linkEntry.findViewById(R.id.btnLinkCancel);
+        dialogEntry.setView(linkEntry);
+        dialogBox = dialogEntry.create();
+        dialogBox.show();
+
+        linkAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!newName.getText().toString().equals("") && !newUrl.getText().toString().equals("")) {
+                    addLink(newName.getText().toString(), newUrl.getText().toString());
+                    Snackbar.make(linkRecyclerView, "Link added", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                }
+                else {
+                    Snackbar.make(linkRecyclerView, "Link could not be added", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+                dialogBox.dismiss();
+            }
+        });
+        linkCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogBox.dismiss();
+                Snackbar.make(linkRecyclerView, "Canceled", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        }
+    }
--- app/src/main/java/edu/northeastern/numad23sp_tylerpoff/LinkViewHolder.java
+++ app/src/main/java/edu/northeastern/numad23sp_tylerpoff/LinkViewHolder.java
@@ -1,10 +1,15 @@
 package edu.northeastern.numad23sp_tylerpoff;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.nio.file.InvalidPathException;
 
 public class LinkViewHolder extends RecyclerView.ViewHolder {
     public TextView nameTV;
@@ -14,6 +19,16 @@ public class LinkViewHolder extends RecyclerView.ViewHolder {
         super(itemView);
         this.nameTV = itemView.findViewById(R.id.name);
         this.urlTV = itemView.findViewById(R.id.url);
+        urlTV.setOnClickListener(view -> {
+            try{
+                String Url = urlTV.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(Url);
+                itemView.getContext().startActivity(intent);
+            }catch(Exception e){
+                Toast.makeText(itemView.getContext(), "Invalid url", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void bindThisData(Link link) {
